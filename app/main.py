fcfb97d66725c776bbd1504c95bfbb5c78e26dde
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2025-08-27 09:56:44 -0300

    corrige nome da variável de URL do banco de dados para manter consistência

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
