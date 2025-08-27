c8d2ee49c189981b727bd20182036a74513e3dcd
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2025-08-27 10:09:53 -0300

    altera configuração da URL do banco de dados para usar PostgreSQL e ajusta a porta padrão

--- app/main.py
+++ app/main.py
@@ -16,14 +16,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 import os
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
-DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
