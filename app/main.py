f6776d9b8c183c7e6c69abca5821b86ee8e029b1
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2025-08-27 09:50:12 -0300

    refatora configuração da URL do banco de dados para usar variáveis de ambiente

--- app/main.py
+++ app/main.py
@@ -15,7 +15,14 @@ from sqlalchemy import (
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 import os
 
-DB_URL = os.getenv("DB_URL", "mysql+pymysql://app:app@localhost:3306/pessoasdb")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
